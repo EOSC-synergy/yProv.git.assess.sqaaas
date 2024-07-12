02a00271cccb79d1be25853b86ece1b7c8ab4bd2
Pablo Orviz <orviz@ifca.unican.es>
2024-07-12 16:08:25 +0200

    Add JePL file structure

--- .sqa/script.zippy-champagne-sponge.sh
+++ .sqa/script.zippy-champagne-sponge.sh
@@ -5,12 +5,5 @@
 
 (
 cd github.com/HPCI-Lab/yProv &&
-    pytest&&
-    docker stop web&&
-    docker rm web&&
-    docker stop db&&
-    docker rm db&&
-    docker volume rm neo4j_data&&
-    docker volume rm neo4j_logs&&
-    docker volume rm yprov_data
+    pytest
 )
