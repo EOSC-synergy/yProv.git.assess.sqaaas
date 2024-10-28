f5f057febe94b5254b00cfffb45170b48538699d
Pablo Orviz <orviz@ifca.unican.es>
2024-10-28 12:24:38 +0100

    Add JePL file structure

--- .sqa/script.grumpy-grey-antelope.sh
+++ .sqa/script.grumpy-grey-antelope.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/HPCI-Lab/yProv &&
-    hadolint Dockerfile hpci-yprov:mocktest/Dockerfile hpci-yprov:1.3/Dockerfile --failure-threshold error
+    hadolint Dockerfile hpci-yprov:1.3/Dockerfile hpci-yprov:mocktest/Dockerfile --failure-threshold error
 )
