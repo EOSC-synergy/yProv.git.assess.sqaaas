80249656240cc7849adfa87a59088d2826b579c9
Pablo Orviz <orviz@ifca.unican.es>
2025-01-16 22:46:45 +0100

    Add JePL file structure

---
+++ .sqa/script.sloppy-turquoise-dachsbracke.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/HPCI-Lab/yProv &&
+    hadolint Dockerfile SQA/Dockerfile SQA/hpci-yprov:1.3/Dockerfile SQA/hpci-yprov:mocktest/Dockerfile --failure-threshold error
+)
